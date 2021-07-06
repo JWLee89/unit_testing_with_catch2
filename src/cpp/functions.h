c2be3977336994cc071b9419b67486f6bffaea9f
jaylee <ljay189@gmail.com>
2021-07-06 18:24:28 +0900

    Added a simple add function to unit test

--- src/cpp/functions.h
+++ src/cpp/functions.h
@@ -1,11 +1,12 @@
+#include <vector>
 
 
-// int sum_vector(const* std::vector<int> vector_to_sum) 
-// {
-//     int sum = 0;
-//     for (auto& num: *vector_to_sum)
-//     {
-//         sum += num;
-//     }
-//     return sum; 
-// }
+int sum_vector(const std::vector<int>* vector_to_sum) 
+{
+    int sum = 0;
+    for (auto& num: *vector_to_sum)
+    {
+        sum += num;
+    }
+    return sum; 
+}
--- test/cpp/test.cpp
+++ test/cpp/test.cpp
@@ -1,11 +1,26 @@
 #include "../../src/cpp/functions.h"
-
-// Catch-related settings. Do not repeat define.
+// Catch-related settings. 
+// Do not repeat definition
 #define CATCH_CONFIG_MAIN
-
-// Each test file requires the following import.
+// Must be included in each unit test. 
 #include "catch.hpp"
 
+// Each test file requires the following import.
+#include <vector>
+
+// Demo test case 
 TEST_CASE("Vector sum is computed correctly", "[vector]") {
+    try 
+    {
+        std::vector<int>* numbers = new std::vector<int>;
+        numbers->push_back(1);
+        numbers->push_back(2);
+        numbers->push_back(3);
+        REQUIRE(sum_vector(numbers) == 6);
+    }
+    catch (std::bad_alloc)
+    {
+        // Do nothing, just skip previous test
+    }
     REQUIRE(1 + 1 == 2);
 }
